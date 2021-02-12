f9442ca1218715dcdec8537cd26d7554d49d6eda
CamiloCJ09 <camilocampazj@gmail.com>
2021-02-11 19:52:37 -0500

    Creating user attributes

--- src/model/UserAccount.java
+++ src/model/UserAccount.java
@@ -4,5 +4,19 @@ public class UserAccount {
 
     private String username;
     private String password;
+    private String gender;
+    private String photoUrl;
+    private String[] carrers;
+    private String birthday;
+    private String favBrowser;
 
+    public UserAccount(String username, String password, String gender, String photoUrl, String[] carrers, String birthday, String favBrowser) {
+        this.username = username;
+        this.password = password;
+        this.gender = gender;
+        this.photoUrl = photoUrl;
+        this.carrers = carrers;
+        this.birthday = birthday;
+        this.favBrowser = favBrowser;
+    }
 }
--- src/ui/ClassroomGUI.java
+++ src/ui/ClassroomGUI.java
@@ -1,20 +1,26 @@
 package ui;
 
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import model.Classroom;
 
+import java.io.File;
 import java.io.IOException;
 
 public class ClassroomGUI {
 
     private Classroom classroom;
 
-    public ClassroomGUI(Classroom classroom){this.classroom = classroom;}
+    public ClassroomGUI(Classroom classroom){
+        this.classroom = classroom;
+    }
 
     @FXML
     private Pane mainPane;
@@ -43,14 +49,23 @@ public class ClassroomGUI {
     @FXML
     private ChoiceBox<String> favoriteBrowser;
 
+
     @FXML
     void browseFileToChoose(ActionEvent event) {
-
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select a image");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"));
+        Stage primaryStage = (Stage)mainPane.getScene().getWindow();
+        File fileToSave = fileChooser.showOpenDialog(primaryStage);
     }
 
     @FXML
     void createAccount(ActionEvent event) {
+        try {
 
+        }catch(Exception ex){
+
+        }
     }
 
     @FXML
@@ -70,6 +85,15 @@ public class ClassroomGUI {
 
         //Parent addContacts = FXMLLoader.load(getClass().getResource("addContact.fxml"));
         mainPane.getChildren().setAll(addContactPane);
+
+
+        favoriteBrowser.getItems().add("Brave");
+        favoriteBrowser.getItems().add("Opera");
+        favoriteBrowser.getItems().add("Chrome");
+        /*
+        favoriteBrowser = new ChoiceBox<String>();
+        favoriteBrowser.getItems().add("Brave");
+         */
     }
     @FXML
     void startApp(ActionEvent event) throws IOException {
